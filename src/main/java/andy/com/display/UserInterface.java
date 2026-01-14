d6c5363a9a6028d17c0e55999cf7d3a943ceed5e
Andy <zhizhongwang_andy@163.com>
2026-01-14 09:38:08 +0800

    feat - 给UserInterface添加分数更改时闪烁下划线的效果

--- src/main/java/andy/com/display/UserInterface.java
+++ src/main/java/andy/com/display/UserInterface.java
@@ -5,6 +5,16 @@ import andy.com.App;
 import andy.com.tools.Config;
 
 public class UserInterface {
+    // 文字闪烁相关变量
+    private int lastScore;
+    private int scoreBlinkingTickNumber;
+    private int lastLength;
+    private int lengthBlinkingTickNumber;
+
+    public UserInterface() {
+        this.scoreBlinkingTickNumber = this.lengthBlinkingTickNumber = 0;
+        this.lastScore = this.lastLength = -1;
+    }
 
     /**
      * 渲染用户界面相关内容
@@ -17,7 +27,32 @@ public class UserInterface {
     }
 
     public String generateScoreAndLength(SnakePlayer snakePlayer) {
-        return String.format("分数 - %d | 长度 - %d\n", snakePlayer.getScore(), snakePlayer.getWholeBodyLength());
+        int currentScore = snakePlayer.getScore();
+        int currentBodyLength = snakePlayer.getWholeBodyLength();
+
+        if (this.lastScore != -1 && this.lastScore != currentScore) {
+            this.scoreBlinkingTickNumber = Config.UI_TEXT_BLINKING_TICK_NUMBER;
+        }
+        if (this.lastLength != -1 && this.lastLength != currentBodyLength) {
+            this.lengthBlinkingTickNumber = Config.UI_TEXT_BLINKING_TICK_NUMBER;
+        }
+
+        this.lastScore = currentScore;
+        this.lastLength = currentBodyLength;
+
+        var scoreANSICode = "";
+        var lengthANSICode = "";
+
+        if (this.scoreBlinkingTickNumber > 0) {
+            this.scoreBlinkingTickNumber--;
+            if (this.scoreBlinkingTickNumber % 2 == 0) { scoreANSICode = "\033[4m"; } // 下划线
+        }
+        if (this.lengthBlinkingTickNumber > 0) {
+            this.lengthBlinkingTickNumber--;
+            if (this.lengthBlinkingTickNumber % 2 == 0) { lengthANSICode = "\033[4m"; }
+        }
+
+        return String.format("分数 - %s%d\033[0m | 长度 - %s%d\033[0m\n", scoreANSICode, currentScore, lengthANSICode, currentBodyLength);
     }
 
     public String generateTimeRemained() {
--- src/main/java/andy/com/tools/Config.java
+++ src/main/java/andy/com/tools/Config.java
@@ -6,5 +6,7 @@ public class Config {
     public static final int FPS = 10;
     public static final int GAME_LENGTH_TICKS = 80 * Config.FPS;
 
+    public static final int UI_TEXT_BLINKING_TICK_NUMBER = 4;
+
     public static final int LINE_NUMBER_ABOVE_GAME_AREA = 5;
 }
